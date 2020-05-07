d18b2a7d674184679ea63b0cf0e8b865a25fff4e
anilchoudhary <masteranilchoudhary@gmail.com>
2020-05-07 21:59:48 +0530

    Animations Part 1

--- src/app/dishdetail/dishdetail.component.ts
+++ src/app/dishdetail/dishdetail.component.ts
@@ -1,6 +1,13 @@
 import { Component, OnInit, ViewChild, Inject } from "@angular/core";
 import { Dish } from "../shared/dish";
 import { DishService } from "../services/dish.service";
+import {
+  trigger,
+  state,
+  style,
+  animate,
+  transition,
+} from "@angular/animations";
 
 import { Params, ActivatedRoute } from "@angular/router";
 import { Location } from "@angular/common";
@@ -12,6 +19,25 @@ import { Comment } from "../shared/Comment";
   selector: "app-dishdetail",
   templateUrl: "./dishdetail.component.html",
   styleUrls: ["./dishdetail.component.scss"],
+  animations: [
+    trigger("visibility", [
+      state(
+        "shown",
+        style({
+          transform: "scale(1.0)",
+          opacity: 1,
+        })
+      ),
+      state(
+        "hidden",
+        style({
+          transform: "scale(0.5)",
+          opacity: 0,
+        })
+      ),
+      transition("* => *", animate("0.5s ease-in-out")),
+    ]),
+  ],
 })
 export class DishdetailComponent implements OnInit {
   @ViewChild("fform") commentFormDirective;
@@ -24,6 +50,7 @@ export class DishdetailComponent implements OnInit {
   commentForm: FormGroup;
   comment: Comment;
   dishcopy: Dish;
+  visibility: ".,shown";
 
   formErrors = {
     comment: "",
@@ -57,13 +84,17 @@ export class DishdetailComponent implements OnInit {
       .subscribe((dishIds) => (this.dishIds = dishIds));
     this.route.params
       .pipe(
-        switchMap((params: Params) => this.dishservice.getDish(params["id"]))
+        switchMap((params: Params) => {
+          this.visibility = "hidden";
+          return this.dishservice.getDish(+params["id"]);
+        })
       )
       .subscribe(
         (dish) => {
           this.dish = dish;
           this.dishcopy = dish;
           this.setPrevNext(dish.id);
+          this.visibility = "shown";
         },
         (errmess) => (this.errMess = <any>errmess)
       );
