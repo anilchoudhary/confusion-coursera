2dc746ac270f699316dcd18d8b9c45a79ba21361
anilchoudhary <masteranilchoudhary@gmail.com>
2020-05-07 19:42:45 +0530

    custom attributes directives

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -38,6 +38,7 @@ import { HomeComponent } from "./home/home.component";
 import { ContactComponent } from "./contact/contact.component";
 import { AppRoutingModule } from "./app-routing/app-routing.module";
 import { LoginComponent } from "./login/login.component";
+import { HighlightDirective } from './directives/highlight.directive';
 
 @NgModule({
   declarations: [
@@ -50,6 +51,7 @@ import { LoginComponent } from "./login/login.component";
     HomeComponent,
     ContactComponent,
     LoginComponent,
+    HighlightDirective,
   ],
   imports: [
     AppRoutingModule,
